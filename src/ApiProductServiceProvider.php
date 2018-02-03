796b05800410c4face8c9f7a27d1a33e7cb69bd8
Dariusz Lubiński <lubinski.dariusz@gmail.com>
2018-02-03 12:38:21 +0100

    skeleton

---
+++ src/ApiProductServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace trancended\ApiProduct;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiProductServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
